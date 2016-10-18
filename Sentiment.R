075d138fe5e6339ff14f9a039fae3acd5077aac8
Spencer Davison <sdavison330@gmail.com>
2016-10-18 09:32:43 -0500

    Add files via upload

---
+++ Sentiment.R
@@ -0,0 +1,107 @@
+library(syuzhet)
+trump <- "Chris, thank you very much. I appreciate it.
+This has been an amazing evening. Already, we’ve won five major states and it looks like we could win six or seven or eight or nine.
+(APPLAUSE)
+It’s really been — it’s really been great.
+I want to congratulation Ted on the winning of Texas. He worked hard on he — I know how hard he worked actually, and so I congratulate Ted Cruz on that win. That was an excellent win.
+We’re going to make America great again, folks. We’re going to make it great again.
+And, you know, I watched Hillary’s speech and she’s talking about wages have been poor and everything’s poor and everything’s doing badly, but we’re going to make it — she’s been there for so long. I mean, if she hasn’t straightened it out by now, she’s not going to straighten it out in the next four years. It’s just going to become worse and worse. She wants to make America whole again and I’m trying to figure out what is that all about. Make America great again is going to be much better than making America whole again.
+So I just want to say that this was an exciting evening. It’s so great to be in Florida. It’s so great to be at Mar-A-Lago with friends and the press and the media and everybody.
+(APPLAUSE)
+I know it was a very tough night for Marco Rubio. He had a tough night but he worked hard. He spent a lot of money. He is a lightweight, as I’ve said many times before. But you know what? We’re going to go to Florida, we’re going to spend so much time in Florida. We’ve got about a 20-point lead. I know that a lot of groups, a lot of the special interests and a lot of the lobbyists and the people that want to have their little senator do exactly as they want, they’re going to put $20 million or $25 million into it over the next two weeks, from what just came over the wires. And frankly, I think that’s fine. As far as I’m concerned, it’s fine. And if he wins, they’ll have totally controlled — total control. But he’s not going anywhere anyway, but we’ll see what happens. But we’re going to spend a lot of time in Florida.
+The Virginia one was just a great win because it’s a place that is just spectacular and a place that we have big investments in, as we have in Florida. Remember, we have thousands of employees in Florida, all along Miami, Doral and Mar-A-Lago and so many other places. And it’s been an amazing place to invest. It’s been amazing to have so many wonderful employees, so many of whom are here tonight, really urging us on.
+And you know, I have thousands of employees all over the country, actually, all over the world but for purposes of tonight, we’ll just say all over the country. And it’s been just so beautiful to watch this company grow and to watch it grow so strongly.
+Recent articles came out talking about how great a company we built, and now we want to put that same ability into doing something for our nation. I mean, our nation is in serious trouble. We’re being chilled on trade, absolutely destroyed. China is just taking advantage of us. I have nothing against China. I have great respect for China, but their leaders are too smart for our leaders. Our leaders don’t have a clue and the trade deficits at $400 billion and $500 are too much. No country can sustain that kind of trade deficit. It won’t be that way for long. We have the greatest business leaders in the world on my team already and, believe me, we’re going to redo those trade deals and it’s going to be a thing of beauty.
+You look at countries like Mexico, where they’re killing us on the border, absolutely destroying us on the border. They’re destroying us in terms of economic development. Companies like Carrier Air Conditioner just moving into Mexico. Ford, moving into Mexico. Nabisco, closing up shop in Chicago and moving into Mexico. We have to stop it, folks. I know how to stop it. We’re going to create jobs. We’re going to create jobs like you’ve never seen.
+We’re going to lower taxes. I have a plan that Larry Kudlow and so many others think is the best plan they’ve seen. We’re going to lower taxes substantially for the middle class. The middle class has been forgotten in our country. It really helped and really probably was the predominant factor in making our country into a country that we all love so much and we’re all so proud of, but we’ve forgotten the middle class. So we’re going to lower taxes.
+We’re lowering taxes on business. You look at all the companies that are moving out. When you see Pfizer moving to Ireland and you see so many other companies, constantly now they’re leaving. They used to move from New York to Florida, or they’d move from New Jersey to someplace else, Chris. But now they’re moving from here — (LAUGHTER) — not that many people are leaving New Jersey. But Chris understands the problem, fully understands. Now they’re leaving from places that they used to move to into other parts of the world. We can’t let that happen.
+We’ve lost our manufacturing jobs. We’ve lost our manufacturing. Millions and millions of jobs, thousands and thousands and thousands of plants, manufacturing plants, warehouses. I mean, we are losing so much. We can’t let it happen.
+I tell the story often about a friend of mine who is in the excavation business and he always orders Caterpillar. And recently, he ordered Komatsu tractors from Japan because they’ve cut the yen. They’ve devalued the yen to such an extent that it was virtually impossible for Caterpillar to compete. And I don’t want that to happen. That’s not going to happen. They shouldn’t be allowed to do it.
+We have tremendous power over everybody because we are really the source. We have great, great power. The problem is we have politicians who truly, truly, truly don’t know what they’re doing. So we’re going to work very, very hard.
+I’m so honored by this evening. If you would’ve told me on June 16th, when I was with my wife, Melania, and we came down the escalator in Trump Tower and it looked, literally, well, a little bit like this. You have a lot of cameras here tonight.
+(APPLAUSE)
+It looked, literally, like the Academy Awards. I never saw so many cameras in my life. And it takes courage to run. I’ll tell you what, it takes a lot of courage to run for president. I’ve never done this before. I’ve been a job-producer. I’ve done a lot of things but this is something I’ve never done, but I felt we had to do it. When you look at the incompetence of the Iran deal, where we give $150 billion, we get absolutely nothing. When you look at all of the problems our country has and you look at our military, which is really being depleted rapidly, depleted. We’re going to make our military bigger and better and stronger than ever before, and nobody, nobody, nobody is going to mess with us, folks, nobody.
+(APPLAUSE)
+So it’s just an amazing honor to have you with me tonight. And if you want, press, I think we told you before we’d be okay if you want to ask a few questions.
+Yes, go ahead."
+
+hillary <- " Thank you all so much. What a Super Tuesday.
+(APPLAUSE)
+You know all across our country today they, Democrats, voted to break down barriers so we can all rise together.
+(APPLAUSE)
+I am so delighted to be here with you in Florida.
+(APPLAUSE)
+I congratulate Senator Sanders on his strong showing and campaigning and I’m grateful to all of you who voted for me, to the volunteers and organizers. I know you’ve worked your hearts out. And to all my friends, many of a lifetime who traveled to all the states to tell people about the candidate they knew. And to the hundreds of thousands of people who went to Hillary Clinton dot com to give what they could, most less than $100.
+Now this campaign moves forward to the Crescent City, the Motor City and beyond. We’re going to work for every vote and we will need all of you to keep volunteering, contributing, doing everything you can – talking to your friends and neighbors. Because this country belongs to all of us, not just those at the top.
+(APPLAUSE)
+Not just the people who look one way, worship one way, or even think one way. America prospers when we all prosper. America is strong when we’re all strong.
+(APPLAUSE)
+And we know we’ve got work to do. But, that work, that work is not to make America great again. America never stopped being great. We need …
+(APPLAUSE)
+We have to make America whole. We have to fill in what’s been hollowed out.
+(CROWD CHANT)
+We have to make strong the broken places, re-stitch the bonds of trust and respect across our country.
+Now it might be unusual, as I’ve said before, for a presidential candidate to say this, but I’m going to keep saying it, “I believe what we need in America today is more love and kindness.”
+(APPLAUSE)
+Because you know what? It works. Instead of building walls we’re going to break down barriers and build … (APPLAUSE)
+… build ladders of opportunity and empowerment so every American can live up to his or her potential, because then and only then can America live up to its full potential too.
+(APPLAUSE)
+Now it’s clear tonight that the stakes in this election have never been higher. And the rhetoric we’re hearing on the other side has never been lower.
+(CROWD CHANT)
+Trying to divide America between us and them is wrong, and we’re not going to let it work.
+(APPLAUSE)
+You know whether we like it or not, we’re all in this together, my friends, and we all have to do our part. But unfortunately, too many of those with the most wealth and the most power in this country today seem to have forgotten that basic truth about America. You know yesterday I was at the Old South Meeting House in Boston where nearly two and a half centuries ago American patriots organized the original Tea Party.
+And I had to wonder what they would make of corporations that seem to have absolutely no loyalty to the country that they love so much. What would they say about student loan companies that overcharge young people struggling to get out of debt. Even young men and women serving our country in the military, or corporations that shift their headquarters overseas to avoid paying their fair share of taxes like Johnson Controls, an auto parts company from Wisconsin that all of us, we taxpayers, helped to bail out with the auto rescue back in 2008. Now they’re turning their back on America.
+(CROWD CHANT)
+Now I’m not interested in condemning whole categories of people or businesses. I’m just interested in making things right. So let there be no doubt, if you cheat your employees, exploit consumers, pollute our environment or rip off the taxpayers, we’re going to hold you accountable.
+(APPLAUSE)
+But, if you do the right thing, if you invest in your workers, and in America’s future then we’ll stand with you. We all need to work together to break down the barriers holding back our families and our country. Because the middle class needs a raise.
+(APPLAUSE)
+And more good jobs. Jobs that pay enough for a family to live on. Even put a little away for retirement. Jobs that provide dignity and a bright future. That’s why we have to invest in manufacturing and infrastructure and small business and clean energy – enough clean energy to power every home in America.
+(APPLAUSE)
+Don’t let anybody tell you we can’t make things in America anymore, because we can, we are, and we will. And together we can break down the barriers that face working class families across America, especially in struggling rust belt communities and small, Appalachian town that have been hollowed out by lost jobs and lost hope. Families who for generations kept our lights on and our factories running. Together we can break down barriers for our kids so they get the education they need and deserve.
+(APPLAUSE)
+Every child in America should have a great school and a great teacher no matter what zip code they live in. Together we can break down barriers for women and finally guarantee equal pay for equal work.
+(CROWD CHANT)
+And we can break down barriers for families who’ve seen too many black children harassed, humiliated, and even killed. WE can break down barriers for voters in North Carolina who’ve been systematically disenfranchised and dis(inaudible).
+(APPLAUSE)
+We can break down barriers for hard-working immigrants everywhere who are too often exploited and intimidated. We have to defend all our rights – workers’ rights, and women’s rights, civil rights and voting rights, LGBT rights and rights for people with disabilities. And that starts by standing with President Obama when he nominates a strong, progressive justice.
+(APPLAUSE)
+I know too many Americans have lost faith in our future. We hear it in the voices of parents who don’t know how they’re going to give their kids the opportunities they deserve. We see it in the eyes of working men and women who don’t expect to come easy but wonder why it has to be quite so hard.
+Like many of you I find strength and purpose in the values I learned from my family and my faith. They gave me simple words to live by, an old Methodist saying. Do all the good you can, for all the people you can, for as long as you can, and that is why
+(APPLAUSE)
+… that is why I believe deeply that if we resist the forces trying to drive us apart we can come together to make this country work for everyone. The struggling, the striving, and the successful, if we all do our part we can restore our common faith in our common future. That’s the spirit powering this campaign. It comes from a young janitor in Arkansas who stopped buying junk food and putting off getting a haircut so he could contribute to it. It comes from the disabled combat veteran from Nebraska who sent in $10. In 70 years of his life he had never donated to a political campaign until now.
+You can join us too. Please go to Hillary Clinton dot com, make a donation, text “join” to 47246, and let me leave you with a story that has inspired so many of us. By now we all know what happened in Flint, Michigan, don’t we. Our city’s children were poisoned by toxic water because their governor wanted to save a little money. But there’s another story in Flint.
+It’s the story of a community that’s been knocked down but refused to be knocked out. It is hundreds of union plumbers coming from across the country to install new water fixtures. It’s students raising funds for water deliveries and showing up to distribute supplies. It’s the United Auto Workers and General Motors donating millions of dollars to help.
+And when I visited Flint a few weeks ago I went to the House of Prayer Missionary Baptist Church. The congregation locked arms and sang, “We’ve come too far from we started from.” They’re not about to quit now. We know there are many other Flints out there. Communities that are hurting and need help. But we’ve come too far in this country to let us turn back.
+We’re gonna build on the progress that we’ve made. We saved the auto industry thanks to President Obama, now we gotta create new jobs and industries of the future.
+(APPLAUSE)
+We now have insured 90 percent of Americans thanks to President Obama and now we’ve got to finish the job and get to 100 percent. We have come too far to stop now. We’ve got to keep going. Keep working. Keep breaking down those barriers and imagine what we can build together when each and every American has a chance to live up to his or her own God-given potential.
+Thank you all so very much. Thank you."
+
+h_sentences <- get_sentences(hillary)
+t_sentences <- get_sentences(trump)
+h_sentiment <- get_sentiment(h_sentences)
+t_sentiment <- get_sentiment(t_sentences)
+
+h_plot <- plot(
+  h_sentiment, 
+  type="l", 
+  main="Hillary", 
+  xlab = "Narrative Time", 
+  ylab= "Emotional Valence"
+)
+
+t_plot <- plot(
+  t_sentiment, 
+  type="l", 
+  main="Trump", 
+  xlab = "Narrative Time", 
+  ylab= "Emotional Valence"
+)
+
+h_nrc <- get_nrc_sentiment(h_sentences)
+t_nrc <- get_nrc_sentiment(t_sentences)
+
+h_sentiment_csv <- data.frame(h_nrc)
+t_sentiment_csv <- data.frame(t_nrc)
